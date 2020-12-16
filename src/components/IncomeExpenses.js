def72e60f5f19336138606417cc693d46ee9f837
skosari <skosari@protonmail.com>
2020-12-16 11:27:51 -0800

    added expense functions

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpenses = () => {
-  
+
+  const { transactions } = useContext(GlobalContext)
+  const amounts = transactions.map(x => x.amount)
+  const income = amounts.filter(x=>x>0).reduce((a,b)=> (a+=b),0).toFixed(2)
+  console.log(income)
+  const expense = amounts.filter(x=>x<0).reduce((a,b)=>(a+=b),0).toFixed(2)
+  console.log(expense)
+
   return(
     <div className='inc-exp-container'>
       <div>
         <h4>Income</h4>
-        <p className='money plus'>+$0.00</p>
+        <p className='money plus'>+${income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className='money minus'>-$0.00</p>
+        <p className='money minus'>-${Math.abs(expense)}</p>
       </div>
     </div>
   )
